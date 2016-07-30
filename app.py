7015bc7997a8501841b3351082e294bdbbdbf485
David Singleton <davidsingleton@gmail.com>
2016-07-30 22:44:51 +0100

    redis hour long cache

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask
 import oer
+import os
+import redis
 
 app = Flask(__name__)
 app.debug = True
 
+appredis = None
+ratefetcher = None
+
+@app.before_first_request
+def init():
+    global appredis, ratefetcher
+    print '+init'
+    appredis = redis.StrictRedis(
+        host=os.environ['REDIS_HOST'], 
+        port=int(os.environ['REDIS_PORT']),
+        db=int(os.environ['REDIS_DB']),
+        password=os.environ['REDIS_KEY'],
+        decode_responses=True)
+    ratefetcher = oer.RateFetcher(appredis)
+    print '-init ', appredis, ratefetcher
+
 @app.route('/<currency>')
 def index(currency):
-    return str(oer.get_rate(currency))
+    return str(ratefetcher.get_rate(currency))
 
--- oer.py
+++ oer.py
@@ -1,20 +1,42 @@
 import os
+import redis
 import requests
 
 KEY = os.environ['OER_KEY']
+HOUR_IN_SECS = 60 * 60
 
-def get_rate(currency):
-	result = ''
-	url = 'https://openexchangerates.org/api/latest.json?app_id=%s' % (KEY)
-	try:
-	  r = requests.get(url, timeout=1)
-	except requests.exceptions.RequestException:
-	  return result
-	if r.status_code == 200:
-		return r.json()['rates'][currency]
-	else:
-		print r.status_code
-	return result
+class RateFetcher(object):
+
+	def __init__(self, redis):
+		self._redis = redis
+
+	def get_rate(self, currency):
+		result = ''
+		result = self._redis.get('fx:USD:' + currency)
+		if not result:
+			self._update_rates()
+			result = self._redis.get('fx:USD:' + currency)
+		return result
+
+	def _update_rates(self):
+		print 'Updating Rates'
+		url = 'https://openexchangerates.org/api/latest.json?app_id=%s' % (KEY)
+		try:
+		  r = requests.get(url, timeout=1)
+		except requests.exceptions.RequestException:
+		  return result
+		if r.status_code == 200:
+			rates =  r.json()['rates']
+			for symbol, value in rates.items():
+				self._redis.set('fx:USD:' + symbol, value, ex=HOUR_IN_SECS)
+		else:
+			print r.status_code
 
 if __name__ == '__main__':
-	print get_rate('GBP')
+	rf = RateFetcher(redis.StrictRedis(
+        host=os.environ['REDIS_HOST'], 
+        port=int(os.environ['REDIS_PORT']),
+        db=int(os.environ['REDIS_DB']),
+        password=os.environ['REDIS_KEY'],
+        decode_responses=True))
+	print rf.get_rate('GBP')
