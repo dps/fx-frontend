d0b1dd859a93671959ad8d6d03085fa9cee92708
David Singleton <davidsingleton@gmail.com>
2016-07-30 22:44:51 +0100

    redis hour long cache

--- run_local.sh
+++ run_local.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+export REDIS_HOST=localhost
+export REDIS_PORT=6379
+export REDIS_KEY=
+export REDIS_DB=0
 export PORT=8000
 export OER_KEY=4930451afa05443999faa94c9abb1394
 gunicorn -b 0.0.0.0:$PORT --log-file=- app:app
