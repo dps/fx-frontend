04e1b0325b0128629f839c07a7e97bf99bda7755
David Singleton <davidsingleton@gmail.com>
2016-07-31 17:55:58 +0100

    don't make underlying requests on favicon.ico and serve metrics

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask import Response
+from werkzeug.routing import BaseConverter
+
 import oer
 import os
 import redis
 
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
 app = Flask(__name__)
+app.url_map.converters['regex'] = RegexConverter
 app.debug = True
 
 appredis = None
@@ -22,7 +32,14 @@ def init():
     ratefetcher = oer.RateFetcher(appredis)
     print '-init ', appredis, ratefetcher
 
-@app.route('/<currency>')
+@app.route('/metrics')
+def metrics():
+	response = ''
+	for key in appredis.keys('fx:meta*'):
+		response = response + key + ' ' + appredis.get(key) + '\n'
+	return Response(response, mimetype='text/plain')
+
+@app.route('/<regex("[A-Z]{3}"):currency>')
 def index(currency):
     return str(ratefetcher.get_rate(currency))
 
